705fbc701177e488d0abd485e454a086291499d6
maple209 <68244345+maple209@users.noreply.github.com>
2020-07-14 17:55:51 +0800

    Add files via upload

---
+++ echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'JMpQ0oYzX/pF1Rd2eAYtLJpHCrldL6UEhuxbtpAhQDBYWy4DqL1YAGphupATc7Zmql9VWm8H01qhFkGBRZTFmYLJfo/PKeu6uWSKy7K1x0Yc9AGfoBn1/SsF9ogtkuC6ieSrcl9pnrK7JFjrJuXJRAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'e0baf7a4ad67f492a31c2ba4d39b247e';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
